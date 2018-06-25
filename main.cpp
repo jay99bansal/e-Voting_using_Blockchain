7e5e7924bb47f72b523af63866a582dcddac2966
Jay Bansal <jay99bansal@gmail.com>
2018-06-25 14:05:01 +0530

    add test

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Blockchain_e-Voting_Project
+//
+//  Created by Jay Bansal on 25/06/18.
+//  Copyright © 2018 Jay Bansal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
